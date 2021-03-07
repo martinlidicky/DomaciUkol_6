a5b00fa217c4b6329dc34c0f18d5d791bb11c8d5
Martin Lidicky <martin.lidicky@gmail.com>
2021-03-07 20:49:45 +0100

    Adding Calculator class

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Calculator {
+
+    enum Operation{
+        ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+    }
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+        Operation operation = Operation.ADDITION;
+
+	    int result = calculate(x, y, operation);
+	    System.out.println("Result: "+result);
+
+    }
+
+    public static int calculate (int x, int y, Operation operation){
+
+        int result = 0;
+        if (operation == Operation.ADDITION) {
+            result = x + y;
+        } else if (operation == Operation.SUBTRACTION) {
+            result = x - y;
+        } else if (operation == Operation.MULTIPLICATION) {
+            result = x * y;
+        } else if (operation == Operation.DIVISION) {
+            result = x / y;
+        }
+        return result;
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DomaciUkol_6
+
+This method allows you to execute 4 basic mathematical operations.
